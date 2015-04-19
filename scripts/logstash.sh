2a152801eb7989653e49f00a9dc5103bd71efdc4
Christian <tinytux@users.noreply.github.com>
2015-04-19 19:14:09 +0200

    vms: installing logstash_1.5.0.rc2-1_all.deb on Debian Jessie 8.0

--- scripts/logstash.sh
+++ scripts/logstash.sh
@@ -8,6 +8,20 @@ if [ $(id -u) != 0 ]; then
     exit 1
 fi
 
+cd /vagrant
 
+DEB_FILE="logstash_1.5.0.rc2-1_all.deb"
+
+if [[ ! -f ${DEB_FILE} ]]; then
+    URL="http://download.elastic.co/logstash/logstash/packages/debian/${DEB_FILE}"
+    echo "wget -q ${URL}"
+    wget -q ${URL}
+fi
+
+
+if [[ ! -e /opt/logstash ]]; then
+    apt-get -y install systemd
+    dpkg -i ${DEB_FILE}
+fi
 
 
